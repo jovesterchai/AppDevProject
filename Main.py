c543ebcec0400fbde41e306d90221533746ff719
jovesterchai <jovesterc@gmail.com>
2020-02-11 02:54:09 +0800

    CreateProduct works but not yet teh  retrivev

--- Main.py
+++ Main.py
@@ -215,12 +215,12 @@ def createProduct():
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['files']
+        file = request.files['file']
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            filename = secure_filename(str(createProductForm.Product.itemID.data + '.jpg'))
+            filename = secure_filename(str(createProductForm.itemID.data + '.jpg'))
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
             item = Product.Product(createProductForm.itemID.data, createProductForm.name.data,
@@ -234,7 +234,7 @@ def createProduct():
             db.close()
 
             return redirect(url_for('retrieveProducts'))
-        return render_template('createProduct.html', form=createProductForm, status='admin')
+    return render_template('createProduct.html', form=createProductForm, status='admin')
 
 
 
